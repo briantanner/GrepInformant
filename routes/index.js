fac94a6f2c827124842188fa9db3df4ca408d05b
Brian Tanner <brian@tanner.io>
2014-05-21 14:53:36 -0400

    remember travel and arrival values after submission

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@
  * GET home page.
  */
 
-var utils = require('Grepolis-Utils');
+var _ = require('underscore'),
+  utils = require('Grepolis-Utils');
 
 exports.index = function(req, res) {
   res.render('index', { title: 'Grepolis Departure Time Calculator' });
@@ -14,7 +15,11 @@ exports.calculate = function (req, res) {
   if (!req.body.travel || !req.body.travel.length) { return res.send(500, 'No travel time provided'); }
   if (!req.body.arrival || !req.body.arrival.length) { return res.send(500, 'No arrival time provided'); }
 
-  var time = utils.getDepartureTime(req.body.travel, req.body.arrival);
+  var time = utils.getDepartureTime(req.body.travel, req.body.arrival),
+    payload = _.extend(req.body, {
+      title: 'Grepolis Departure Time Calculator',
+      departure: time
+    });
 
-  res.render('index', { title: 'Grepolis Departure Time Calculator', departure: time });
+  res.render('index', payload);
 };
