58bc4fb5df70fd0cee07259579b64bed7ae845e2
Brian Tanner <brian@tanner.io>
2015-03-09 03:43:54 -0400

    added WW ranking table

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.get('/', routes.compare);
 app.post('/', routes.calculate);
 app.get('/compare', routes.compare);
 app.get('/conquers', routes.conquers);
+app.get('/rank', routes.rank);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
